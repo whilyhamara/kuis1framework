b0c9933c49f8d410f349c82866b3a6bd7d5beb8d
whilyhamara <whilyhamanjasmara@gmail.com>
2017-03-03 15:44:02 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>About< e/title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<h1 class="text-center">Whilyham Blog</h1>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand">Whilyham</a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="home">Home</a></li>
+						<li><a href="about">About</a></li>
+						<li><a href="contact">Contact</a></li>
+					</ul>
+					
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<tbody align="right">
+						<tr>
+							<td>Nama</td>
+						</tr>
+						<tr>
+							<td>NIM</td>
+						</tr>
+						<tr>
+							<td>Alamat</td>
+						</tr>
+						<tr>
+							<td>No Telepon</td>
+						</tr>
+						<tr>
+							<td>Agama</td>
+						</tr>
+						<tr>
+							<td>Jenis Kelamin</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Whilyham Anjasmara</th>
+						</tr>
+						<tr>
+							<td>1541180043</td>
+						</tr>
+						<tr>
+							<td>Jombang</td>
+						</tr>
+						<tr>
+							<td>085730093469</td>
+						</tr>
+						<tr>
+							<td>Islam</td>
+						</tr>
+						<tr>
+							<td>Laki-laki</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
